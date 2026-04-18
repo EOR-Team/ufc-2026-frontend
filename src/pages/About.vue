d22693421f2302fc8b6b4d96fcdfdf6c510bf3c5
n1ghts4kura <1825259836@qq.com>
2026-04-18 22:11:53 +0800

    feat: add Vue app source files

---
+++ src/pages/About.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <v-main>
+    <v-container>
+      <h1>About</h1>
+      <p class="text-body-1 mt-4">
+        UFC 2026 is your ultimate destination for UFC event tracking and fighter statistics.
+      </p>
+    </v-container>
+  </v-main>
+</template>
